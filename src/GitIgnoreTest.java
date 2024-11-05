104b00ba1e441a80a6bf7312084719535192d232
bsmmutarrif <mutarrif.nbr@gmail.com>
2024-11-05 21:28:36 +0600

    two string add

--- src/GitIgnoreTest.java
+++ src/GitIgnoreTest.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
 
 public class GitIgnoreTest {
     public static void main(String[] args) {
-// remove comments and add your code here
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first string: ");
+        String str1 = scanner.nextLine();
+
+        System.out.print("Enter the second string: ");
+        String str2 = scanner.nextLine();
+
+
+        String joinedString = str1 + str2;
+        System.out.println("The joined string is: " + joinedString);
+
+        scanner.close();
     }
 }
